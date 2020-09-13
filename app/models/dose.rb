a87f03405d1c9d2fa83010ac64de4337d1e793c7
aarongolden <goldenaaron@gmail.com>
2020-09-13 13:11:15 +0900

    mister cocktail solution

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, :cocktail, :ingredient, presence: true
+  validates :ingredient, uniqueness: { scope: :cocktail }
+  # for every cocktail, you can only use the ingredient once-under scope-uniqueness
+  # a dose belongs to a cocktail- AND a cocktail HAS MANY doses
 end
